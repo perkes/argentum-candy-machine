242a182ba045009c7821ae1ba7b3887ab992586d
perkes <jonathanperkes@gmail.com>
2022-07-02 14:10:50 -0300

    Removing footer

--- src/Home.tsx
+++ src/Home.tsx
@@ -201,25 +201,6 @@ const Home = (props: HomeProps) => {
             {alertState.message}
           </Alert>
         </Snackbar>
-        <div className="footer">
-          <div className="buttons">
-            <a target="_blank" rel="noreferrer" href="https://twitter.com/HeroesArgentum">
-              <img className="logo" src="./twitter-logo.png" width="32" height="32" alt="twitter"></img>
-            </a> 
-            <a target="_blank" rel="noreferrer" href="https://discord.gg/w7xpsxYaB7">
-              <img className="logo" src="./discord-logo.png" width="32" height="32" alt="discord"></img>
-            </a>
-            <a target="_blank" rel="noreferrer" href="https://solscan.io/account/Cb9TfAyWW4DpThYfwnuJ4mhsGTEK6iLaZypLD3eA6vQp">
-              <img className="logo" src="./solscan-logo.png" width="32" height="32" alt="solscan"></img>
-            </a>
-            <a target="_blank" rel="noreferrer" href="https://github.com/martinabenedictis/heroesofargentum">
-              <img className="logo" src="./github-logo.png" width="32" height="32" alt="github"></img>
-            </a>
-            <a target="_blank" rel="noreferrer" href="https://t.co/biOV6MEfDJ">
-              <img className="logo" src="./solsea-logo.png" width="32" height="32" alt="solsea"></img>
-            </a>
-          </div>
-        </div>
       </td>
       <td className="container">
         <table>
